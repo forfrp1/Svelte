a55ddae6f922612d43bb6dc99b55e639a78ec3e0
frp <=>
2025-09-27 08:13:49 +0000

    video 31

---
+++ sk-loading-data/src/routes/products/+layout.js
@@ -0,0 +1,7 @@
+export const load = async (loadEvent) => {
+    const {fetch} = loadEvent;
+    const title = "Featured products"
+    const response = await fetch("http://localhost:4000/featured-products")
+    const featuredProducts = await response.json()
+    return {title, featuredProducts}
+}
---
+++ sk-loading-data/src/routes/products/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let { data, children } = $props();
+  const { title, featuredProducts } = data;
+</script>
+{@render children?.()}
+<h1>{title}</h1>
+
+{#each featuredProducts as fProduct}
+<div class="inline-block w-52 m-3 border border-slate-300 p-3">
+    <h2>{fProduct.title}</h2>
+    <p>{fProduct.description}</p>
+</div>
+{/each}
