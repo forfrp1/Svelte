87ce65195a54f2992b2e61301594ccc8729f591e
thisismammad <thisismamamd7@gmail.com>
2025-09-29 16:20:43 +0330

    video 36-37

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex py-4 flex-col gap-2">
-  <a href="/products">Products</a>
+  <a href="/products" >Products</a>
   <a href="/stocks">Stocks</a>
 </div>
--- sk-loading-data/src/routes/products/product.svelte
+++ sk-loading-data/src/routes/products/product.svelte
@@ -4,7 +4,7 @@
 
 <div class="my-4 mx-4 border-b px-4 py-1 w-max">
   <h1 class="text-2xl hover:text-slate-600 font-bold">
-    <a href={"/products/" + product.id}>{product.title}</a>
+    <a href={`/products/${product.id}`}>{product.title}</a>
   </h1>
   <p class="text-blue-600">${product.price}</p>
   <p class="text-slate-500">{product.description}</p>
