7ea87086716dc0b322a05b6bb9798369dd3ecf90
thisismammad <thisismamamd7@gmail.com>
2025-09-29 17:19:41 +0330

    video 43

---
+++ sk-loading-data/src/routes/+page.js
@@ -0,0 +1,7 @@
+export const load = async () => {
+  console.log("Home page universal load function called");
+};
+
+export const prerender = true;
+// turn off client side rendering
+export const csr = false;
