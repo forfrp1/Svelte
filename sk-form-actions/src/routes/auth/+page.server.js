f1fc5f62be28d4c8170a41cf4b7be35a5bb038ac
thisismammad <thisismamamd7@gmail.com>
2025-10-01 21:35:33 +0330

    video 48

---
+++ sk-form-actions/src/routes/auth/+page.server.js
@@ -0,0 +1,17 @@
+// export const load = ()=>{
+
+// }
+
+export const actions = {
+  default: async ({ request, cookies }) => {
+    const data = await request.formData();
+    const username = data.get("username");
+    const password = data.get("password");
+    if (!username || !password) {
+      return { message: "Missing username or password" };
+    }
+    // @ts-ignore
+    cookies.set("username", username, { path: "/" });
+    return { message:"Logged in"}
+  },
+};
--- sk-form-actions/src/routes/auth/+page.svelte
+++ sk-form-actions/src/routes/auth/+page.svelte
@@ -1,37 +1,22 @@
 <script lang="ts">
-  let username = "";
-  let password = "";
-  let message = "";
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    message = "";
-    const response = await fetch("/auth", {
-      method: "POST",
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    });
-    const data = await response.json();
-    message = data.message;
-  };
+let {form} = $props()
 </script>
 
-<form class="flex flex-col w-72 mx-auto gap-2" onsubmit={handleSubmit}>
-  <p>{message}</p>
+<form method="POST" class="flex flex-col w-72 mx-auto gap-2" >
+  <p>{form?.message|| ""}</p>
   <input
     type="text"
     class="bg-blue-200 p-2"
     name="username"
     placeholder="Username"
-    bind:value={username}
+    
   />
   <input
     type="password"
     class="bg-blue-200 p-2"
     name="password"
     placeholder="Password"
-    bind:value={password}
+    
   />
   <button
     class="cursor-pointer hover:bg-red-500 duration-200 bg-red-400 p-1"
